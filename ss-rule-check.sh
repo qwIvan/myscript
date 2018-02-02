30f6f6b62c0dd0e9669e895d1e1d8e2583fd99bc
ivan <qwIvan@users.noreply.github.com>
2018-02-03 00:16:45 +0800

    update

--- ss-rule-check.sh
+++ ss-rule-check.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 iptables-save | grep -q ss_rules || /etc/init.d/shadowsocks-libev reload
+iptables -t mangle -D PREROUTING -p udp --dport 5938 -j RETURN
+iptables -t mangle -I PREROUTING -p udp --dport 5938 -j RETURN
