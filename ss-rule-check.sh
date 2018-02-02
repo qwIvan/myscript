fa85228ea34262043a70601b9f1c0d47ade24f6d
ivan <qwIvan@users.noreply.github.com>
2018-02-03 01:09:31 +0800

    update

--- ss-rule-check.sh
+++ ss-rule-check.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 iptables-save | grep -q ss_rules || /etc/init.d/shadowsocks-libev reload
+iptables -t mangle -D ss_rules_pre_src -p udp --dport 5938 -j RETURN
+iptables -t mangle -I ss_rules_pre_src -p udp --dport 5938 -j RETURN
