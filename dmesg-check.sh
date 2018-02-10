32a46cd5d249b336680d63cad8bf733e2d269e8c
ivan <qwIvan@users.noreply.github.com>
2018-02-10 16:55:58 +0800

    修改：     dmesg-check.sh

--- dmesg-check.sh
+++ dmesg-check.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-dmesg | grep 'Dropping frame due to full tx queue' && date >> /root/reboot.log && reboot
+dmesg | grep 'Dropping frame due to full tx queue' && ! [ $(nc 192.168.1.101 8090) = '1' ] && date >> /root/reboot.log && reboot
